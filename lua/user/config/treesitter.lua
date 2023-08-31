e02482e3a24b3f6cc5fb2bc619ac3844364d55d1
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-08-31 20:48:37 +0900

    feat: disable treesitter on large file

--- lua/user/config/treesitter.lua
+++ lua/user/config/treesitter.lua
@@ -1,9 +1,4 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
-  return
-end
-
-configs.setup({
+require("nvim-treesitter.configs").setup({
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "vimdoc", "lua", "c", "java", "python", "rust", "toml" },
 
@@ -26,16 +21,17 @@ configs.setup({
     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
+    --
     -- list of language that will be disabled
-    disable = { "" }, -- list of language that will be disabled
+    -- disable = { "" }, -- list of language that will be disabled
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    -- disable = function(lang, buf)
-    --     local max_filesize = 100 * 1024 -- 100 KB
-    --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-    --     if ok and stats and stats.size > max_filesize then
-    --         return true
-    --     end
-    -- end,
+    disable = function(lang, bufnr)
+      local is_large = vim.api.nvim_buf_line_count(bufnr) > 50
+      if is_large then
+        print("Tree-sitter disabled due to code size.")
+        return true
+      end
+    end,
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
