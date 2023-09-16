29a8396e9ea75036b31afe57bc2caa5ea0a8eeb8
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-09-16 19:28:43 +0900

    fix(lazy): incompatible lazy loading

--- lua/user/plugins/dap.lua
+++ lua/user/plugins/dap.lua
@@ -1,7 +1,6 @@
 local M = {
   {
     "mfussenegger/nvim-dap",
-    event = { "BufReadPre", "BufNewFile", "InsertEnter" },
     dependencies = { { "rcarriga/nvim-dap-ui", opts = {} } },
     config = function()
       require("user.config.dap")
