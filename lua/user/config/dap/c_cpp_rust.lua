085057d352753f452a0e6ac8112d5999e969669b
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-09-26 10:00:24 +0900

    feat(dap): use codelldb as c and c++ debugger

--- lua/user/config/dap/c_cpp_rust.lua
+++ lua/user/config/dap/c_cpp_rust.lua
@@ -1,51 +1,30 @@
 local dap = require("dap")
 
-dap.adapters.lldb = {
-  type = "executable",
-  command = "/usr/bin/lldb-vscode", -- adjust as needed, must be absolute path
-  name = "lldb",
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = "codelldb",
+    args = { "--port", "${port}" },
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  },
 }
 
--- TODO: Add support for codelldb or cpptools
-
-local cpp_conf = {
-  name = "Launch",
-  type = "lldb",
+local codelldb_conf = {
+  name = "Launch file",
+  type = "codelldb",
   request = "launch",
   program = function()
     return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
   end,
   cwd = "${workspaceFolder}",
   stopOnEntry = false,
-  args = {},
-
-  -- 💀
-  -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-  --
-  --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-  --
-  -- Otherwise you might get the following error:
-  --
-  --    Error on launch: Failed to attach to the target process
-  --
-  -- But you should be aware of the implications:
-  -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-  -- runInTerminal = false,
 }
 
-local attach_conf = {
-  -- If you get an "Operation not permitted" error using this, try disabling YAMA:
-  --  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-  name = "Attach to process",
-  type = "lldb", -- Adjust this to match your adapter name (`dap.adapters.<name>`)
-  request = "attach",
-  pid = require("dap.utils").pick_process,
-  args = {},
-}
-
-dap.configurations.cpp = { cpp_conf }
-
--- If you want to use this for Rust and C, add something like this:
+dap.configurations.cpp = { codelldb_conf }
 dap.configurations.c = dap.configurations.cpp
 
 local rust_conf = {
@@ -70,4 +49,4 @@ local rust_conf = {
   end,
   -- ...,
 }
-dap.configurations.rust = { vim.tbl_deep_extend("keep", cpp_conf, rust_conf) }
+dap.configurations.rust = { vim.tbl_deep_extend("keep", codelldb_conf, rust_conf) }
--- lua/user/config/mason_tool_installer.lua
+++ lua/user/config/mason_tool_installer.lua
@@ -1,6 +1,6 @@
 local debuggers = {
   -- c, cpp, rust
-  "cpptools",
+  "codelldb",
   -- python
   "debugpy",
   -- java
