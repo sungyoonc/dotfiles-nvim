12cea04afc8e72e4a2bae91a888d03ec158f5b00
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-09-29 17:29:42 +0900

    refactor: replace deprecated function

--- lua/user/config/dap/java.lua
+++ lua/user/config/dap/java.lua
@@ -3,7 +3,7 @@
 -- https://github.com/mfussenegger/nvim-jdtls/blob/095dc490f362adc85be66dc14bd9665ddd94413b/lua/jdtls/util.lua#L5
 local function execute_command(command, callback, bufnr)
   local clients = {}
-  local candidates = vim.lsp.get_active_clients({ bufnr = bufnr })
+  local candidates = vim.lsp.get_clients({ bufnr = bufnr }) or vim.lsp.get_active_clients({ bufnr = bufnr })
   for _, c in pairs(candidates) do
     local command_provider = c.server_capabilities.executeCommandProvider
     local commands = type(command_provider) == "table" and command_provider.commands or {}
