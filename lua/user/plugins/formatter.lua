9118e41b0273ec05edd94b0e43cfb1239f8211c3
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-10-16 12:53:39 +0900

    feat(lazy): lazy load conform

--- lua/user/plugins/formatter.lua
+++ lua/user/plugins/formatter.lua
@@ -1,8 +1,23 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    config = function ()
-      require("user.config.formatter")
-    end
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo", "Format" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>lf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
   },
+  config = function()
+    require("user.config.formatter")
+  end,
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
