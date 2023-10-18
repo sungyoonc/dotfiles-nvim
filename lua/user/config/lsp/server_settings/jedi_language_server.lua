b1fb6d28f9e430a488d1225f8b5a000e2b171c4d
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-10-18 20:14:21 +0900

    fix(autopairs): use pyright completion
    Due to https://github.com/windwp/nvim-autopairs/pull/404 disabling pyright's completionProvider throws error when using nvim-cmp. This is a temporary fix, and will have to look into it.

--- lua/user/config/lsp/server_settings/jedi_language_server.lua
+++ lua/user/config/lsp/server_settings/jedi_language_server.lua
@@ -1,6 +1,8 @@
 -- Use ":lua =vim.lsp.buf_get_clients()"
 -- to get current buffer's language server options
 return {
+  completionProvider = false, -- Disabling to be compatible with autopairs
+  -- https://github.com/windwp/nvim-autopairs/pull/404
   definitionProvider = false,
   documentHighlightProvider = false,
   documentSymbolProvider = false,
--- lua/user/config/lsp/server_settings/pyright.lua
+++ lua/user/config/lsp/server_settings/pyright.lua
@@ -1,7 +1,7 @@
 -- Use ":lua =vim.lsp.buf_get_clients()"
 -- to get current buffer's language server options
 return {
-  completionProvider = false,
   hoverProvider = false,
   signatureHelpProvider = false,
 }
+
