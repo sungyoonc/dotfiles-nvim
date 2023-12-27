b360f829fdab06d2e84af75d66718c940c6b36e7
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-12-27 21:44:36 +0900

    fix(ftplugin): tab config to follow docs

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,4 +1,4 @@
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,4 +1,4 @@
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
