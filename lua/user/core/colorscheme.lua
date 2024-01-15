294d8550cad91700277074f1ad7250e871f2b7b1
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-01-15 13:28:59 +0900

    feat: use undercurl

--- lua/user/core/colorscheme.lua
+++ lua/user/core/colorscheme.lua
@@ -6,3 +6,11 @@ catch /^Vim\%((\a\+)\)\=:E185/
   set background=dark
 endtry
 ]])
+
+vim.cmd([[
+hi DiagnosticUnderlineError gui=undercurl
+hi DiagnosticUnderlineWarn gui=undercurl
+hi DiagnosticUnderlineInfo gui=undercurl
+hi DiagnosticUnderlineHint gui=undercurl
+hi DiagnosticUnderlineOk gui=undercurl
+]])
