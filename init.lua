aa4df7fead28d0895d973cc96941c81c59534f0d
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-01-16 18:05:25 +0900

    feat: move colorscheme config to plugins file

--- init.lua
+++ init.lua
@@ -1,3 +1,2 @@
 require("core").setup()
 require("core.lazy") -- Load lazy.nvim
-require("core.colorscheme")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,25 @@
+
+---@param name string
+local function apply(name)
+  vim.cmd(string.format([[colorscheme %s]], name))
+
+  vim.cmd([[
+    hi DiagnosticUnderlineError gui=undercurl
+    hi DiagnosticUnderlineWarn gui=undercurl
+    hi DiagnosticUnderlineInfo gui=undercurl
+    hi DiagnosticUnderlineHint gui=undercurl
+    hi DiagnosticUnderlineOk gui=undercurl
+  ]])
+end
 return {
   -- catppuccin
-  { "catppuccin/nvim", name = "catppuccin", lazy = true },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      apply("catppuccin-mocha")
+    end,
+  },
 }
