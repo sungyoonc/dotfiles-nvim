278388e36a95c0026391c43ac10348a832368c3a
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-01-16 18:47:40 +0900

    feat(treesitter): add nvim-treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,17 @@
+local function get_upvalue(func, name)
+  local i = 1
+  while true do
+    local n, v = debug.getupvalue(func, i)
+    if not n then
+      break
+    end
+    if n == name then
+      return v
+    end
+    i = i + 1
+  end
+end
+
 return {
   -- treesitter
   {
@@ -10,5 +24,27 @@ return {
     config = function()
       require("config.treesitter")
     end,
+  },
+  -- show context of the current function
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    enabled = true,
+    opts = { mode = "cursor", max_lines = 3 },
+    keys = {
+      {
+        "<leader>ut",
+        function()
+          local tsc = require("treesitter-context")
+          tsc.toggle()
+          if get_upvalue(tsc.toggle, "enabled") then
+            vim.notify("Enabled Treesitter Context", vim.log.levels.INFO, { title = "Option" })
+          else
+            vim.notify("Disabled Treesitter Context", vim.log.levels.WARN, { title = "Option" })
+          end
+        end,
+        desc = "Toggle Treesitter Context",
+      },
+    },
   },
 }
