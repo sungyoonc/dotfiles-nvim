2820b807f445bd3ff91cbebeafa8a304a0ca8cd5
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-01-17 20:38:32 +0900

    feat: add harpoon

---
+++ lua/config/harpoon.lua
@@ -0,0 +1,14 @@
+local harpoon = require("harpoon")
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-s>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-k>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-j>", function() harpoon:list():next() end)
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "ThePrimeagen/harpoon",
+    event = "VeryLazy",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("config.harpoon")
+    end,
+  },
+  {
     "echasnovski/mini.files",
     keys = {
       {
