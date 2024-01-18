1334cfd4d25afafee086f7933391a7d698d16907
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-01-18 23:44:24 +0900

    fix(harpon): keybinds not supported
    <C-;> is not supported.
    <C-j> and <C-S-j> are recognized as the same.

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -4,11 +4,11 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-s>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<M-j>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-k>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-l>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<M-;>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-k>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-j>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<M-S-k>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<M-S-j>", function() harpoon:list():next() end)
