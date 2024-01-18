b041149a4c23167127e3d4fceb18ffb06912acac
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-01-18 18:02:07 +0900

    fix(treesitter): update hyprlang parser name

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,16 +1,16 @@
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.hypr = {
+parser_config.hyprlang = {
   install_info = {
-    url = "https://github.com/luckasRanarison/tree-sitter-hypr",
+    url = "https://github.com/luckasRanarison/tree-sitter-hyprlang",
     files = { "src/parser.c" },
     branch = "master",
   },
-  filetype = "hypr",
+  filetype = "hyprlang",
 }
 
 require("nvim-treesitter.configs").setup({
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "vimdoc", "lua", "c", "java", "python", "rust", "toml", "hypr" },
+  ensure_installed = { "vimdoc", "lua", "c", "java", "python", "rust", "toml", "hyprlang" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
