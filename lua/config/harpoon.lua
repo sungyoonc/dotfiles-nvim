cb287f757e727847a9dadb360924d223bf6a6d8d
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-03-11 21:53:17 +0900

    feat(harpoon): use ctrl for keymaps
    Use control as the modifier for harpoon keymaps. In conjunction with
    alacritty settings, Control+; is now possible.

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -9,23 +9,27 @@ keymap("n", "<C-s>", function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
 end, { desc = "Toogle Harpoon Menu" })
 
-keymap("n", "<M-j>", function()
+keymap("n", "<C-j>", function()
   harpoon:list():select(1)
 end, { desc = "Harpoon 1" })
-keymap("n", "<M-k>", function()
+keymap("n", "<C-k>", function()
   harpoon:list():select(2)
 end, { desc = "Harpoon 2" })
-keymap("n", "<M-l>", function()
+keymap("n", "<C-l>", function()
   harpoon:list():select(3)
 end, { desc = "Harpoon 3" })
-keymap("n", "<M-;>", function()
+-- Real terminal does not recognize Ctrl-;
+-- But terminal emulators(alacritty) can send ANSI escape code magic to neovim, so it can use C-;
+-- Control Sequence Introducer + Unicode Decimal + Modifier + u (Stands for unicode)
+-- \u001b[                       59              ; 5          u
+keymap("n", "<C-;>", function()
   harpoon:list():select(4)
 end, { desc = "Harpoon 4" })
 
 -- Toggle previous & next buffers stored within Harpoon list
-keymap("n", "<M-S-k>", function()
+keymap("n", "<C-S-K>", function()
   harpoon:list():prev()
 end, { desc = "Harpoon Previous" })
-keymap("n", "<M-S-j>", function()
+keymap("n", "<S-NL>", function() -- <S-NL> is <C-S-J>
   harpoon:list():next()
 end, { desc = "Harpoon Next" })
