ece33088b1e957d8dba20d65adf557e9e32bbc63
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-03-19 10:23:34 +0900

    chore(nvim-dap-ui): add new dependency

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,7 +1,13 @@
 return {
   {
     "mfussenegger/nvim-dap",
-    dependencies = { { "rcarriga/nvim-dap-ui", opts = {} } },
+    dependencies = {
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+        opts = {},
+      },
+    },
     config = function()
       require("config.dap")
     end,
