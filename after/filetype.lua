d03d63dce74dd4c953293dae6aa9aa76bc83f348
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-04-24 14:43:45 +0900

    feat(filetype): add hyprlang for .hl

--- after/filetype.lua
+++ after/filetype.lua
@@ -1,11 +1,17 @@
 -- For more info on filetype visit https://neovim.io/doc/user/lua.html#vim.filetype.add()
 -- For more patterns visit https://neovim.io/doc/user/luaref.html#lua-pattern
 
+local pattern = {}
+local filename = {}
+
+-- hyprlang
+pattern[".*/hypr.*/.*%.conf"] = "hyprlang" -- hyprland configs
+filename[".*%.hl"] = "hyprlang" -- other hyprlang configs
+
+-- leetcode.nvim
+filename["leetcode.nvim"] = "leetcode.nvim"
+
 vim.filetype.add({
-  pattern = {
-    [".*/hypr/.*%.conf"] = "hyprlang",
-  },
-  filename = {
-    ["leetcode.nvim"] = "leetcode.nvim",
-  },
+  pattern = pattern,
+  filename = filename,
 })
